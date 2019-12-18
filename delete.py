d03a4dded0eef0d88c07b886a8321f0d6004ede8
54kennywang <qibo.wang@mail.utoronto.ca>
2019-12-18 12:35:49 -0500

    need to understand Target/data1_x_calc

--- delete.py
+++ delete.py
@@ -1,30 +1,230 @@
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+#load packages
+import os
+dirname = os.path.dirname(__file__)
+import sys #access to system parameters https://docs.python.org/3/library/sys.html
+print("Python version: {}". format(sys.version))
+
+import pandas as pd #collection of functions for data processing and analysis modeled after R dataframes with SQL like features
+print("pandas version: {}". format(pd.__version__))
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+import matplotlib #collection of functions for scientific and publication-ready visualization
+print("matplotlib version: {}". format(matplotlib.__version__))
+
+import numpy as np #foundational package for scientific computing
+print("NumPy version: {}". format(np.__version__))
+
+import scipy as sp #collection of functions for scientific computing and advance mathematics
+print("SciPy version: {}". format(sp.__version__))
+
+import sklearn #collection of machine learning algorithms
+print("scikit-learn version: {}". format(sklearn.__version__))
+
+#misc libraries
+import random
+import time
+
+#ignore warnings
 import warnings
 warnings.filterwarnings('ignore')
-import numpy as np
+print('-'*25)
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "./input"]).decode("utf8"))
+
+# Common Model Algorithms
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from xgboost import XGBClassifier
+
+#Common Model Helpers
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+
+#Visualization
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+# from pandas.tools.plotting import scatter_matrix
+
+#Configure Visualization Defaults
+#%matplotlib inline = show plots in Jupyter Notebook browser
+mpl.style.use('ggplot')
+sns.set_style('white')
+pylab.rcParams['figure.figsize'] = 12,8
+
+# display missing-ness
+def display_missing(df):
+    for col in df.columns.tolist():
+        print( '{} column missing values: {}/{} = {}'.format(col, df[col].isnull().sum(), df.shape[0], df[col].isnull().sum()/df.shape[0]))
+    print('\n')
+
+def correlation_heatmap(df):
+    _, ax = plt.subplots(figsize=(14, 12))
+    colormap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    _ = sns.heatmap(
+        df.corr(),
+        cmap=colormap,
+        square=True,
+        cbar_kws={'shrink': .9},
+        ax=ax,
+        annot=True,
+        linewidths=0.1, vmax=1.0, linecolor='white',
+        annot_kws={'fontsize': 12}
+    )
+
+    plt.title('Pearson Correlation of Features', y=1.05, size=15)
+    plt.show()
+
+def fill_in_missingness(data_cleaner):
+    for dataset in data_cleaner:
+        # complete missing age with median
+        dataset['Age'].fillna(dataset['Age'].median(), inplace=True)
+
+        # complete embarked with mode
+        dataset['Embarked'].fillna(dataset['Embarked'].mode()[0], inplace=True)
+
+        # complete missing fare with median
+        dataset['Fare'].fillna(dataset['Fare'].median(), inplace=True)
+    return data_cleaner
+
+def feature_engineering(data_cleaner):
+    for dataset in data_cleaner:
+        # Discrete variables
+        dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
+        dataset['IsAlone'] = 1  # initialize to yes/1 is alone
+        dataset['IsAlone'].loc[dataset['FamilySize'] > 1] = 0  # now update to no/0 if family size is greater than 1
+
+        # quick and dirty code split title from name: http://www.pythonforbeginners.com/dictionary/python-split
+        dataset['Title'] = dataset['Name'].str.split(", ", expand=True)[1].str.split(".", expand=True)[0]
+
+        # Continuous variable bins; qcut vs cut: https://stackoverflow.com/questions/30211923/what-is-the-difference-between-pandas-qcut-and-pandas-cut
+        # Fare Bins/Buckets using qcut or frequency bins: https://pandas.pydata.org/pandas-docs/stable/generated/pandas.qcut.html
+        dataset['FareBin'] = pd.qcut(dataset['Fare'], 4)
+
+        # Age Bins/Buckets using cut or value bins: https://pandas.pydata.org/pandas-docs/stable/generated/pandas.cut.html
+        dataset['AgeBin'] = pd.cut(dataset['Age'].astype(int), 5)
+    return data_cleaner
+
+def name_cleanUp(data1):
+    # cleanup rare title names
+    # print(data1['Title'].value_counts())
+    stat_min = 10  # while small is arbitrary, we'll use the common minimum in statistics: http://nicholasjjackson.com/2012/03/08/sample-size-is-10-a-magic-number/
+    title_names = (data1[
+                       'Title'].value_counts() < stat_min)  # this will create a true false series with title name as index
+
+    # apply and lambda functions are quick and dirty code to find and replace with fewer lines of code: https://community.modeanalytics.com/python/tutorial/pandas-groupby-and-python-lambda-functions/
+    data1['Title'] = data1['Title'].apply(lambda x: 'Misc' if title_names.loc[x] == True else x)
+    return data1
+
+def categorical_to_ordinal(data_cleaner):
+    # code categorical data
+    label = LabelEncoder()  # generate number values for categorical data
+    for dataset in data_cleaner:
+        dataset['Sex_Code'] = label.fit_transform(dataset['Sex'])
+        dataset['Embarked_Code'] = label.fit_transform(dataset['Embarked'])
+        dataset['Title_Code'] = label.fit_transform(dataset['Title'])
+        dataset['AgeBin_Code'] = label.fit_transform(dataset['AgeBin'])
+        dataset['FareBin_Code'] = label.fit_transform(dataset['FareBin'])
+    return data_cleaner
+
+def Model():
+    data_raw = pd.read_csv('./input/train.csv')
+
+    # a dataset should be broken into 3 splits: train, test, and (final) validation
+    # the test file provided is the validation file for competition submission
+    # we will split the train set into train and test data in future sections
+    data_val = pd.read_csv('./input/test.csv')
+
+    # to play with our data we'll create a copy
+    # remember python assignment or equal passes by reference vs values, so we use the copy function: https://stackoverflow.com/questions/46327494/python-pandas-dataframe-copydeep-false-vs-copydeep-true-vs
+    data1 = data_raw.copy(deep=True)
+
+    # however passing by reference is convenient, because we can clean both datasets at once
+    data_cleaner = [data1, data_val]
+
+    # display_missing(data1) # age/carbin/embark
+    # display_missing(data_val) # age/carbin/fare/embark
+
+    data_cleaner = fill_in_missingness(data_cleaner)
+    # display_missing(data1) # cabin/
+    # display_missing(data_val) # cabin
+
+    # delete the cabin feature/column and others previously stated to exclude in train dataset
+    # drop_column = ['PassengerId', 'Cabin', 'Ticket']
+    # data1.drop(drop_column, axis=1, inplace=True)
+
+    data_cleaner = feature_engineering(data_cleaner)
+
+    data1 = name_cleanUp(data1)
+
+    data_cleaner = categorical_to_ordinal(data_cleaner)
 
 
-d1 = {'Sex': ['male', 'female']}
-d1 = pd.DataFrame(data=d1)
-print(d1)
-label = LabelEncoder()
-d1['Sex_Code'] = label.fit_transform(d1['Sex'])
-print(d1)
 
-data1_x = ['Sex']
-data1_x_calc = ['Sex_Code']
-Target = ['Survived']
-data1_xy =  Target + data1_x
-print('Original X Y: ', data1_xy, '\n')
 
-data1_x_bin = ['Sex_Code']
-data1_xy_bin = Target + data1_x_bin
-print('Bin X Y: ', data1_xy_bin, '\n')
+    # define y variable aka target/outcome
+    Target = ['Survived']
+    # define x variables for original features aka feature selection
+    data1_x = ['Sex', 'Pclass', 'Embarked', 'Title', 'SibSp', 'Parch', 'Age', 'Fare', 'FamilySize',
+               'IsAlone']  # pretty name/values for charts
+    data1_x_calc = ['Sex_Code', 'Pclass', 'Embarked_Code', 'Title_Code', 'SibSp', 'Parch', 'Age',
+                    'Fare']  # coded for algorithm calculation
+    data1_xy = Target + data1_x
+    print('Original X Y: ', data1_xy, '\n')
 
-data1_dummy = pd.get_dummies(d1[data1_x])
-print(data1_dummy, '\n')
-data1_x_dummy = data1_dummy.columns.tolist()
-print(data1_x_dummy, '\n')
-data1_xy_dummy = Target + data1_x_dummy
-print('Dummy X Y: ', data1_xy_dummy, '\n')
+    # define x variables for original w/bin features to remove continuous variables
+    data1_x_bin = ['Sex_Code', 'Pclass', 'Embarked_Code', 'Title_Code', 'FamilySize', 'AgeBin_Code', 'FareBin_Code']
+    data1_xy_bin = Target + data1_x_bin
+    print('Bin X Y: ', data1_xy_bin, '\n')
+
+    # define x and y variables for dummy features original
+    data1_dummy = pd.get_dummies(data1[data1_x])  # having one-hot effect
+    data1_x_dummy = data1_dummy.columns.tolist()  # col names
+    data1_xy_dummy = Target + data1_x_dummy
+    print('Dummy X Y: ', data1_xy_dummy, '\n')
+
+
+
+    # As mentioned previously, the test file provided is really validation data for competition submission.
+    # So, we will use sklearn function to split the training data in two datasets; 75/25 split.
+    # This is important, so we don't overfit our model. Meaning split train and test data with function defaults
+    # random_state -> seed or control random number generator: https://www.quora.com/What-is-seed-in-random-number-generation
+    train1_x, test1_x, train1_y, test1_y = model_selection.train_test_split(data1[data1_x_calc], data1[Target],
+                                                                            random_state=0)
+    train1_x_bin, test1_x_bin, train1_y_bin, test1_y_bin = model_selection.train_test_split(data1[data1_x_bin],
+                                                                                            data1[Target],
+                                                                                            random_state=0)
+    train1_x_dummy, test1_x_dummy, train1_y_dummy, test1_y_dummy = model_selection.train_test_split(
+        data1_dummy[data1_x_dummy], data1[Target], random_state=0)
+
+    print("Data1 Shape: {}".format(data1.shape))
+    print("Train1 Shape: {}".format(train1_x.shape))
+    print("Test1 Shape: {}".format(test1_x.shape))
+
+    # print(data_cleaner[0].head(3))
+    # print(data_cleaner[1].head(3))
+
+
+
+
+
+if __name__== "__main__":
+    Model()
+
+
+
+
+
+
+
+
+
